3f7129d3274c600609d11f6835c5d10087b019d0
Ventii <53571300+ventiiapp@users.noreply.github.com>
2019-08-29 12:03:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
   }));
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`server running on port ${PORT}`);
+    console.log(`server running on port #${PORT}`);
 });
 
 app.get('/', (req, res) => {
